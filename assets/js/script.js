47daf01393ae42b4a408f2c76fe7d8d05ac57f64
George Saad <gsparqcode@gmail.com>
2022-04-02 14:38:36 -0400

    added fetch call to retrive data, displayed forecast days

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 //adding variables connected to elements
-
+var APIKEY = "741b0aef8d0031f234fb37ddb9280e81";
 var dateEl = document.querySelector("#date");
 var date = moment().format("l");
 //get today's day value (1-29 for most days) and turn into integer
 var todayNum = parseInt(date.split("/")[1]);
+var counter = 1;
 
-counter = 1;
+//set up date
+dateEl.textContent = "(" + date + ")";
 
+//display next 5 dates on each element
 var getFiveDays = function () {
   for (let i = todayNum - 1; i <= 5; i++) {
     var nextDay = moment(moment(), "L").add(i, "days").format("L");
@@ -17,6 +20,14 @@ var getFiveDays = function () {
   }
 };
 getFiveDays();
-//get date
 
-dateEl.textContent = "(" + date + ")";
+fetch(
+  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" +
+    APIKEY
+).then(function (response) {
+  if (response.ok) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  }
+});
